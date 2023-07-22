a7d70fb5af9d05cd4c72cab85b3629b119d4eee5
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-23 00:34:40 +0700

    fix: image size

--- src/modules/Category/ItemGridCard.tsx
+++ src/modules/Category/ItemGridCard.tsx
@@ -2,7 +2,7 @@ import { useAppContext } from "@/core/contexts";
 import { Ingredients } from "@/core/contexts/ingredients";
 import { Item } from "@/core/types";
 import { body2Sizes } from "@/core/utils/fontSizes";
-import { Card, Flex, Progress, Spacer, Text } from "@chakra-ui/react";
+import { Box, Card, Flex, Progress, Spacer, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -29,49 +29,47 @@ export const ItemsGridCard: FC<ItemsGridCardProps> = ({ item }) => {
                 }}
                 display={
                     ingredient.name.includes(searchText.toLowerCase())
-                        ? "block"
+                        ? "flex"
                         : "none"
                 }
             >
-                <Flex direction="column">
-                    <Image
-                        alt="ingredient"
-                        src={ingredient.image}
-                        style={{
-                            objectFit: "cover",
-                            flexShrink: 0,
-                            minWidth: "100%",
-                            minHeight: "100%",
-                        }}
-                    />
+                <Image
+                    alt="ingredient"
+                    src={ingredient.image}
+                    style={{
+                        objectFit: "cover",
+                        flexShrink: 0,
+                        minWidth: "100%",
+                        minHeight: "50%",
+                        maxHeight: "50%",
+                    }}
+                />
+                <Text mt={2} fontSize={body2Sizes}>
+                    {ingredient.name.charAt(0).toUpperCase() +
+                        ingredient.name.slice(1)}
+                </Text>
+                <Text mt={2} fontSize={body2Sizes} fontWeight="bold">
+                    {item.price} ฿/{ingredient.unit}
+                </Text>
+                <Progress
+                    value={(item.currentAmount / item.goal) * 100}
+                    bgColor="tertiary.700"
+                />
+                <Flex>
                     <Text mt={2} fontSize={body2Sizes}>
-                        {ingredient.name.charAt(0).toUpperCase() +
-                            ingredient.name.slice(1)}
+                        เหลือ{" "}
+                        {Math.ceil(
+                            Math.abs(
+                                item.deadline.getTime() - new Date().getTime()
+                            ) /
+                                (1000 * 60 * 60 * 24)
+                        )}{" "}
+                        วัน
                     </Text>
-                    <Text mt={2} fontSize={body2Sizes} fontWeight="bold">
-                        {item.price} ฿/{ingredient.unit}
+                    <Spacer />
+                    <Text mt={2} fontSize={body2Sizes}>
+                        {item.currentAmount}/{item.goal} {ingredient.unit}
                     </Text>
-                    <Progress
-                        value={(item.currentAmount / item.goal) * 100}
-                        bgColor="tertiary.700"
-                    />
-                    <Flex>
-                        <Text mt={2} fontSize={body2Sizes}>
-                            เหลือ{" "}
-                            {Math.ceil(
-                                Math.abs(
-                                    item.deadline.getTime() -
-                                        new Date().getTime()
-                                ) /
-                                    (1000 * 60 * 60 * 24)
-                            )}{" "}
-                            วัน
-                        </Text>
-                        <Spacer />
-                        <Text mt={2} fontSize={body2Sizes}>
-                            {item.currentAmount}/{item.goal} {ingredient.unit}
-                        </Text>
-                    </Flex>
                 </Flex>
             </Card>
         )
