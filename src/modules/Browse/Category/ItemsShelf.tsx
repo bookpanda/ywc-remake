e85a8c9ab6d3d41c8fdcc615cfe355f997f07971
Idhibhat Pankam <marginpankam@gmail.com>
2023-07-22 23:06:06 +0700

    feat: search feat

--- src/modules/Browse/Category/ItemsShelf.tsx
+++ src/modules/Browse/Category/ItemsShelf.tsx
@@ -2,16 +2,35 @@ import { Flex } from "@chakra-ui/react";
 import { FC } from "react";
 import { ItemsRow } from "./ItemsRow";
 import { Category } from "@/core/types";
+import { useAppContext } from "@/core/contexts";
+import { Ingredients } from "@/core/contexts/ingredients";
 
 export const ItemsShelf: FC = () => {
+    const { searchText, items } = useAppContext();
+    const categories = [
+        Category.Meat,
+        Category.Vegatables,
+        Category.Fruits,
+        Category.Grains,
+        Category.Dairy,
+        Category.Spices,
+    ];
     return (
-        <Flex direction="column" gap={5} mt={20}>
-            <ItemsRow category={Category.Meat} />
-            <ItemsRow category={Category.Vegatables} />
-            <ItemsRow category={Category.Fruits} />
-            <ItemsRow category={Category.Grains} />
-            <ItemsRow category={Category.Dairy} />
-            <ItemsRow category={Category.Spices} />
+        <Flex direction="column" gap={5} mt={10}>
+            {categories.map((c, index) => {
+                const ingredientsInCat = Ingredients.filter(
+                    (i) => i.category === c
+                );
+                const searchedIngredients = ingredientsInCat.filter((i) =>
+                    i.name.includes(searchText.toLowerCase())
+                );
+
+                return (
+                    searchedIngredients.length > 0 && (
+                        <ItemsRow key={index} category={c} />
+                    )
+                );
+            })}
         </Flex>
     );
 };
